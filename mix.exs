3a6120ccfd6982b946f39dc22a94a1d1b69cb733
Frerich Raabe <frerich.raabe@gmail.com>
2023-07-25 23:49:38 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FilterFormatter.MixProject do
   def project do
     [
       app: :filter_formatter,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
