83ead248592979d82aa39634de7626518c1b20e2
Frerich Raabe <frerich.raabe@gmail.com>
2023-07-25 23:24:24 +0200

    Lower required Elixir version to 1.14
    Requiring the latest and greatest Elixir version was maybe a little
    harsh, and it make the 1.14 CI builds fail. Let's be a little more
    relaxed here.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FilterFormatter.MixProject do
     [
       app: :filter_formatter,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
